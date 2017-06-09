e64ffdb002ad6c9a6c3a57e466588101c94386c0
Matt Dalton <matt@finimize.com>
2017-06-09 18:00:49 +0100

    created empty display graph component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CurrencyInput from './components/CurrencyInput'
 import SliderInput from './components/SliderInput'
+import DisplayGraph from './components/DisplayGraph'
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
         <div className="header-banner">
           <h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
         </div>
-				<div className="financial-content">
+				<div className="financial-inputs">
 					<p className="input-label">How much have you saved?</p>
 					<CurrencyInput defaultValue={0}/>
 
@@ -19,6 +20,26 @@ class App extends Component {
 
 					<p className="input-label">How much interest will you earn per year?</p>
 					<SliderInput defaultValue={4}/>
+				</div>
+				<div className="financial-display">
+					<DisplayGraph data={[
+						{
+							year: 1,
+							amount:500
+						},
+						{
+							year: 2,
+							amount:700
+						},
+						{
+							year: 3,
+							amount:1000
+						},
+						{
+							year: 4,
+							amount:1500
+						}
+					]}/>
 				</div>
       </div>
     );
---
+++ src/components/DisplayGraph.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default class DisplayGraph extends Component {
+
+	render() {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
+
+DisplayGraph.propTypes = {
+	data: PropTypes.arrayOf(PropTypes.object)
+}
