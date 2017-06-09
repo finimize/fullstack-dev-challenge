91336a38b157b12b44f64078d982dfb5a8290d78
Matt Dalton <matt@finimize.com>
2017-06-09 17:04:22 +0100

    created basic currency input and added first questions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CurrencyInput from './components/CurrencyInput'
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,12 @@ class App extends Component {
         <div className="header-banner">
           <h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<div className="financial-content">
+					<p className="input-label">How much have you saved?</p>
+					<CurrencyInput defaultValue={0}/>
+					<p className="input-label">How much will you save each month?</p>
+					<CurrencyInput defaultValue={0}/>
+				</div>
       </div>
     );
   }
