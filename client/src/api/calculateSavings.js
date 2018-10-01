cdf3661f835bfd15842cb1c8a5f42490ec77ce23
Ciprian Caba <caba.ciprian@gmail.com>
2018-10-01 22:17:01 +0100

    feature: Retrieve data from server on the client

--- client/src/api/calculateSavings.js
+++ client/src/api/calculateSavings.js
@@ -1,8 +1,19 @@
-import { CALCULATE_SAVINGS_RESULT } from './stubs/calculateSavings'
+import { apolloClient } from '../apolloClient'
+import gql from 'graphql-tag'
 
-export const calculateSavings = ({
-  initialSavings,
-  monthlySavings,
-  yearlyInterest,
-  interestRecurrency,
-}) => Promise.resolve(CALCULATE_SAVINGS_RESULT)
+export const calculateSavings = variables => {
+  const query = gql`
+    {
+      calculations: calculateSavings(
+        initialSavings: 0
+        monthlySavings: 0
+        yearlyInterest: 0
+        interestRecurrency: Monthly
+      ) {
+        month
+        amount
+      }
+    }
+  `
+  return apolloClient.query({ query, variables }).then(res => res.data)
+}
