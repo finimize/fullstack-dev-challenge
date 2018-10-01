949e7acde4f44726b22b383a1542b891938b0ef7
Ciprian Caba <caba.ciprian@gmail.com>
2018-10-01 20:27:41 +0100

    feature: Implemented basic state management with debounced updates

--- client/src/components/FinancialInput.js
+++ client/src/components/FinancialInput.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import CurrencyInput from './CurrencyInput'
+import PropTypes from 'prop-types'
+import { CurrencyInput } from './CurrencyInput'
 import SliderInput from './SliderInput'
 
 export class FinancialInput extends React.PureComponent {
+  state = {
+    initialSavings: 0,
+    monthlySavings: 0,
+    yearlyInterest: 4,
+    interestMonthlyRecurrency: 1,
+  }
+
+  componentDidUpdate = () => {
+    this.props.onVariablesUpdated(this.state)
+  }
+
   render() {
     return (
       <div className="financial-inputs">
         <p className="input-label">How much have you saved?</p>
-        <CurrencyInput defaultValue={0} />
+        <CurrencyInput
+          value={this.state.initialSavings}
+          onChange={initialSavings => this.setState({ initialSavings })}
+        />
 
         <p className="input-label">How much will you save each month?</p>
-        <CurrencyInput defaultValue={0} />
+        <CurrencyInput
+          value={this.state.monthlySavings}
+          onChange={monthlySavings => this.setState({ monthlySavings })}
+        />
 
         <p className="input-label">How much interest will you earn per year?</p>
         <SliderInput defaultValue={4} />
       </div>
     )
   }
+}
+
+FinancialInput.propTypes = {
+  onVariablesUpdated: PropTypes.func,
 }
--- client/src/components/FinimizeInterestRateCalculator.js
+++ client/src/components/FinimizeInterestRateCalculator.js
@@ -1,14 +1,19 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { FinancialInput } from './FinancialInput'
 import { Header } from './Header'
 import { FinancialDisplay } from './FinancialDisplay'
+import { debounce } from '../utils/debounce'
 
 export class FinimizeInterestRateCalculator extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <FinancialInput />
+        <FinancialInput
+          onVariablesUpdated={debounce(variables => {
+            console.log('debounce', variables)
+          }, 1000)}
+        />
         <FinancialDisplay />
       </div>
     )
