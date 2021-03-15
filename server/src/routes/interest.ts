3dfc00749411fd47455edf5d955b882fe8a94bdb
Elroy Antao <elroyantao@gmail.com>
2021-03-15 00:24:09 +0000

    fix validation

--- server/src/routes/interest.ts
+++ server/src/routes/interest.ts
@@ -9,7 +9,7 @@ const router = express.Router()
 router.post('/compound', joiValidate({
   body: {
     principal: Joi.number().min(0).max(1000000).required(),
-    interestRate: Joi.number().min(0).max(19).required(),
+    interestRate: Joi.number().min(0).max(20).required(),
     monthlyDeposit: Joi.number().min(0).max(10000).required(),
     period: Joi.number().min(1).max(10000)
   }
