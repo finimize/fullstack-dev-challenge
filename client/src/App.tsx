f086b73f7a643d45a674a6e07688f5e945aae1fd
mark-carrington <mark@finimize.com>
2022-01-27 16:03:51 +0000

    Revert "Julian's changes"
    This reverts commit 3714790fb70aa5da8705acd24342acc586630cca.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,61 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
-import { Input } from './components/Input'
 import DefaultLayout from './components/layouts/Default'
 import LineChart from './components/LineChart'
 import theme from './theme'
 
 const defaultTheme = extendTheme(theme)
 
-const initialData = {
-    year: [],
-    amount: [],
+// Note: This is just for example purposes
+// should be replaced with real data from the server
+const tempData = {
+    xAxis: [0, 1, 2, 3, 4, 5],
+    yAxis: [100, 150, 180, 210, 240, 350],
 }
 
-const App = () => {
-    const [initialValue, setInitialValue] = useState('1000')
-    const [monthlyDeposit, setMonthlyDeposit] = useState('100')
-    const [interestRate, setInterestRate] = useState('3')
-    const [data, setData] = useState(initialData)
-
-    useEffect(() => {
-        fetch(
-            `http://localhost:3001/calculation?initial=${initialValue}&monthlyDeposit=${monthlyDeposit}&interestRate=${interestRate}`
-        )
-            .then((response) => response.json())
-            .then((responseJson) => {
-                setData(responseJson.data)
-            })
-    }, [initialValue, monthlyDeposit, interestRate])
-
+function App() {
     return (
         <ChakraProvider theme={defaultTheme}>
+            {/* We've just bundled everything into one file here to 
+            get you started!*/}
             <DefaultLayout>
                 <Container pt={6}>
-                    <Input
-                        title="How much are you starting with?"
-                        defaultValue={initialValue}
-                        onChange={(val) => setInitialValue(val)}
-                        testId="initialValueInput"
-                    />
-                    <Input
-                        title="How much can you put aside monthly?"
-                        defaultValue={monthlyDeposit}
-                        onChange={(val) => setMonthlyDeposit(val)}
-                        testId="monthlyDepositInput"
-                    />
-                    <Input
-                        title="What percentage interest rate can you get?"
-                        defaultValue={interestRate}
-                        onChange={(val) => setInterestRate(val)}
-                        testId="interestRateInput"
-                    />
                     <LineChart
                         title="Savings Over time"
-                        xAxisData={data.year}
-                        yAxisData={data.amount}
+                        xAxisData={tempData.xAxis}
+                        yAxisData={tempData.yAxis}
                         xLabel="Years"
                         yLabel="Amount"
                     />
--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from "express";
-import type { Request, Response } from "express";
 
-export const app = express();
+const app = express();
 
 app.set("port", process.env.PORT || 3001);
 
@@ -9,60 +8,6 @@ app.set("port", process.env.PORT || 3001);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-type Projection = {
-  year: Array<number>;
-  amount: Array<number>;
-};
-
-type RequestParams = {
-  initial: string;
-  monthlyDeposit: string;
-  interestRate: string;
-};
-
-type ResponseType = {
-  data: Projection;
-};
-
-const calculationHandler = (
-  req: Request<{}, {}, {}, RequestParams>,
-  res: Response<ResponseType>
-) => {
-  const initial = parseFloat(req.query.initial);
-  const monthlyDeposit = parseFloat(req.query.monthlyDeposit);
-  const interestRate = parseFloat(req.query.interestRate) / 100;
-
-  const data: Projection = { year: [], amount: [] };
-
-  const monthlyInterestRate = interestRate / 12;
-
-  for (let year = 0; year < 50; year++) {
-    const months = year * 12;
-    const yearAmount =
-      (monthlyDeposit * (Math.pow(1 + monthlyInterestRate, months) - 1)) /
-        monthlyInterestRate +
-      initial * Math.pow(1 + monthlyInterestRate, months);
-
-    data.year.push(year);
-    data.amount.push(yearAmount);
-  }
-
-  res.json({
-    data: data,
-  });
-};
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Adding this so I can run server and client on localhost
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.get("/calculation", calculationHandler);
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
