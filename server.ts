ffc40dda201db2d17af6d6ea2130b0789c84a1c4
Finimize <finimize@Finimizes-MacBook-Pro.local>
2022-07-05 18:52:20 +0100

    small upgrades

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.set("port", process.env.PORT || 3001);
 
-// Express only serves static assets in production
+// Express only serves static assets in production 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
